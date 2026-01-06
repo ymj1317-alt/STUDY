4e4ba6556a80945ff855f76d71bff14464a834eb
ymj1317-alt <ymj1317@gmail.com>
2026-01-06 23:07:08 +0900

    Add files via upload

---
+++ C++/01-1/BetweenAdder.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main(void)
+{
+	int val1, val2;
+	int result = 0;
+
+	std::cout<< "두 개의 숫자입력 : ";
+	std::cin >> val1 >> val2;		//두개의 정수를 입력받아서 val1, val2에 순서대로 저장할것을 명령하는 문장
+
+	if (val1 < val2)
+	{
+		for (int i = val1 + 1; i < val2; i++)		//for문 내에서 변수를 선언할수 있음
+		{
+			result += i;
+		}
+	}
+	else
+	{
+		for (int i = val2 + 1; i < val2; i++)		//for문 내에서 변수를 선언할수 있음
+		{
+			result += i;
+		}
+	}
+
+	std::cout << "두 수 사이의 정수 합 : " << result << std::endl;
+
+	return 0;
+}
