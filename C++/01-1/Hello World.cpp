b4d50008c4976a97d4dd6571691d59a866a6d5b5
ymj1317-alt <ymj1317@gmail.com>
2026-01-06 21:30:34 +0900

    Add files via upload

--- C++/01-1/Hello World.cpp
+++ C++/01-1/Hello World.cpp
@@ -1,1 +1,13 @@
+﻿#include <iostream>
 
+int main(void)
+{
+	int num = 20;
+
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello " << "World!" << std::endl;
+	std::cout << num << ' ' << 'A';
+	std::cout << ' ' << 3.14 << std::endl;
+
+	return 0;
+}
