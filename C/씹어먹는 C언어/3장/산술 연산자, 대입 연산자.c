5fbf6a3fbd8891624b1558b0de02f8490c17f9ac
ymj1317-alt <ymj1317@gmail.com>
2026-01-08 15:59:01 +0900

    Add files via upload

---
+++ C/씹어먹는 C언어/3장/산술 연산자, 대입 연산자.c
@@ -0,0 +1,16 @@
+#include <stdio.h>	
+
+int main(void)
+{
+	int a, b;
+	a = 10;
+	b = 3;
+
+	printf("a + b는 : %d\n", a + b);
+	printf("a - b는 : %d\n", a - b);
+	printf("a * b는 : %d\n", a * b);
+	printf("a / b는 : %d\n", a / b);
+	printf("a % b는 : %d\n", a % b);
+
+	return 0;
+}
