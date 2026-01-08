2b247c06d2a4767d1549b845251613871c7b1670
ymj1317-alt <ymj1317@gmail.com>
2026-01-08 11:30:57 +0900

    Add files via upload

---
+++ C/씹어먹는 C언어/2장/변수 선언하기1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>	
+
+int main(void)
+{
+	int a;
+
+	a = 127;
+
+	printf("a의 값은 %d 진수로 %o 입니다.\n", 8, a);
+	printf("a의 값은 %d 진수로 %d 입니다.\n", 10, a);
+	printf("a의 값은 %d 진수로 %x입니다.\n", 16, a);
+
+	return 0;
+}
