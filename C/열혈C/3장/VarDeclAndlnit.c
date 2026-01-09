c9f59bb8caecb034fbbf3c4c563e7d0344bed4c8
ymj1317-alt <ymj1317@gmail.com>
2026-01-09 15:45:06 +0900

    Add files via upload

---
+++ C/열혈C/3장/VarDeclAndlnit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>	
+
+int main(void)
+{
+	int num1, num2;
+	int num3 = 30, num4 = 40;
+
+	printf("num1 : %d, num2 : %d\n", num1, num2);
+
+	num1 = 10;
+	num2 = 20;
+
+	printf("num1 : %d, num2 : %d\n", num1, num2);
+	printf("num3 : %d, num4 : %d\n", num3, num4);
+
+	return 0;
+}
