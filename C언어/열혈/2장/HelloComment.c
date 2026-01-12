d27b303b6d36f3d33aadcc71b6ea8ac8e5b682fa
ymj1317-alt <ymj1317@gmail.com>
2026-01-12 15:47:53 +0900

    Add files via upload

---
+++ C언어/열혈/2장/HelloComment.c
@@ -0,0 +1,19 @@
+/*
+제목 : Hello world 출력하기
+기능 : 문자열의 출력
+파일 이름 : HelloComment.c
+수정 날짜 : 2026.01.12
+작성자 : 윤성우
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	/*이 함수 내에서는 하나의 문자열을 출력한다.
+	문자열은 모니터로 출력된다.
+	*/
+	printf("Hello world!\n");		//문자열의 출력
+
+	return 0;						//0의 반환
+}									//main함수의 끝
