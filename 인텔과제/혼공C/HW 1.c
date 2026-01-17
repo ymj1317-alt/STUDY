3941b313af602ca9a1db178091ffb4f966e64373
ymj1317-alt <ymj1317@gmail.com>
2026-01-17 15:47:30 +0900

    Add files via upload

---
+++ 인텔과제/혼공C/HW 1.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    double light_year = 3 * 365 * 24 * 36 * 1e-5;
+    double distance_year = (40) / light_year;       //1e-12이 분자 분모에 공통적으로 있어 미리 제거
+    double vo_light = (6 * 24 * 365) / (light_year * 1e8);
+    double vo_time = distance_year / vo_light;   //걸리는 시간=주어진 거리(광년)/보이저(광년)속도
+    printf("보이저로 가려면 얼마의 시간이 걸리는가?[광년] : %.1lf[광년]\n", vo_time);
+
+    return 0;
+}
