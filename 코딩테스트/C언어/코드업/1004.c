04db3c7bcf5b961fadc749356b13d669cd8dd7dc
ymj1317-alt <ymj1317@gmail.com>
2026-01-20 09:05:38 +0900

    Add files via upload

---
+++ 코딩테스트/C언어/코드업/1004.c
@@ -0,0 +1,15 @@
+/*이번에는 작은 따옴표(single quotation mark)가 들어있는
+특수한 형태의 출력문에 대한 연습을 해보자.
+
+다음 문장을 출력하시오.
+
+'Hello'*/
+
+#include <stdio.h>	
+
+int main(void)
+{
+	printf("\'Hello\'\n");
+
+	return 0;
+}
